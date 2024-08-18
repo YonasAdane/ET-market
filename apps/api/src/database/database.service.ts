90436b11bf21b081ba419fb52fd9a7c2c9ef6390
YonasAdane <yonasadane94@gmail.com>
2024-08-18 15:51:43 +0300

    user service updated

--- apps/api/src/database/database.service.ts
+++ apps/api/src/database/database.service.ts
@@ -2,7 +2,7 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@repo/database/src';
 
 @Injectable()
-export class DatabaseService extends PrismaClient implements OnModuleInit {
+export class Prisma extends PrismaClient implements OnModuleInit {
     async onModuleInit() {
         await this.$connect();
     }
--- apps/api/src/user/user.service.ts
+++ apps/api/src/user/user.service.ts
@@ -1,26 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { Prisma } from 'src/database/database.service';
+import { Prisma as PrismaSchema } from '@repo/database/src';
 @Injectable()
 export class UserService {
+  constructor(private readonly prisma:Prisma){}
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.prisma.user.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: number): Promise<object | null> {
+    return this.prisma.user.findFirst({ where: { id } });
   }
-
   update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: number):Promise<any> {
+    return this.prisma.user.delete({where:{id}});
   }
 }
