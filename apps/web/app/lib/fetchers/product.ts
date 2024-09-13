6cb3a23c8607935cccffc942848e4e48f85dc848
YonasAdane <yonasadane94@gmail.com>
2024-09-13 13:08:51 +0300

    integrate remote api with web app

--- apps/web/app/lib/fetchers/product.ts
+++ apps/web/app/lib/fetchers/product.ts
@@ -6,16 +6,13 @@ export async function findProducts(categoryType:string,queryParams:any){
     try {
         console.log("request param obj: ",queryParams);
         
-        let baseUrl=`http://localhost:4000/api/v1/products/${categoryType}`
+        let baseUrl=`${process.env.BACKEND_URL}/api/v1/products/${categoryType}`
         const products=await axios.get(baseUrl, {
             params: queryParams,
             headers: {
               'Content-Type': 'application/json'
             }
           })
-
-          
-        // const products=await fetch(`http://localhost:4000/api/v1/products/${categoryType}${queryParams}`,{cache:"no-store"});
         return products.data;
         
     } catch (error) {
@@ -24,7 +21,7 @@ export async function findProducts(categoryType:string,queryParams:any){
 }
 export async function findProductsBrand(categoryType:string){
     try {
-        const brands=await fetch(`http://localhost:4000/api/v1/products/${categoryType}/brands-categories`);
+        const brands=await fetch(`${process.env.BACKEND_URL}/api/v1/products/${categoryType}/brands-categories`);
         return brands.json();
         
     } catch (error) {
